30021d72d37a6118626ced9ed3b55a38cdb8cda7
nikhilmathewfrancis98 <nikhilmathewfrancis@gmail.com>
2021-10-17 12:16:14 +0530

    Project started Splash screen added

---
+++ MiniProjectSem5/app/src/main/java/com/example/opentable/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.opentable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, HomeActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
